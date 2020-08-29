32753f3843c4eaba0dfd148dd7fa075fd9d98953
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-29 23:19:50 +0300

    Test application

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include "TGM.h"
 
 int main() {
-    std::cout << "Hello World!\n";
+	tgm::Ecosystem ecosystem;
+	auto grass = ecosystem.AddSpecies("grass", 100, { 1.01f, 1.0f });
+	auto bunny = ecosystem.AddSpecies("bunny", 100, { 1.05f, 0.98f });
+	bunny->AddImpactOn("grass", 0.01f);
+	int counter = 0;
+	ecosystem.PrintSpeciesPopulations("bunny", "grass");
+	std::cin.get();
+	while (counter < 100) {
+		ecosystem.Update();
+		ecosystem.PrintSpeciesPopulations("bunny", "grass");
+		++counter;
+		std::cin.get();
+	}
 }

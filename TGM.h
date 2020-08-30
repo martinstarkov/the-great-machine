278af02fc3311f61b928623a67ed119d2f6200a3
Nicole Burg <32913338+nicolerburg@users.noreply.github.com>
2020-08-30 12:00:43 -0400

    New basic framework

--- TGM.h
+++ TGM.h
@@ -24,6 +24,76 @@ namespace tgm {
         float mutation_chance;
     };
 
+    class Species {
 
+    public:
+        Species() = delete;
+        Species(const std::string& name, Population population, Genes&& genes) : name{ name }, population{ population }, genes{ genes } {}
+
+        const std::string& GetName() const {
+            return name;
+        }
+
+        const Population GetPopulation() const {
+            return population;
+        }
+
+        void Update() {
+
+        }
+
+    private:
+        const std::string name;
+        Population population;
+        Genes genes;
+
+    };
+
+    class Ecosystem {
+
+    public:
+        Ecosystem() = default;
+
+        template <typename ...TArgs>
+        std::shared_ptr<Species> AddSpecies(TArgs&&... args) {
+            auto species = std::make_shared<Species>(std::forward<TArgs&&>(args)...);
+            all_species.emplace(species->GetName(), species);
+            return species;
+        }
+
+        void RemoveSpecies(const std::string& name) {
+            all_species.erase(name);
+        }
+
+        std::shared_ptr<Species> HasSpecies(const std::string& name) const {
+            auto it = all_species.find(name);
+            if (it != std::end(all_species)) {
+                return it->second;
+            }
+            return nullptr;
+        }
+
+        void PrintAllSpecies() {
+            for (auto& pair : all_species) {
+                LOG(pair.first);
+            }
+        }
+
+        void PrintSpeciesPopulations() {
+            for (auto& pair : all_species) {
+                LOG(pair.first << " : " << pair.second->GetPopulation());
+            }
+        }
+
+        void Update() {
+            for (auto& pair : all_species) {
+                pair.second->Update();
+            }
+        }
+    
+    private:
+        std::unordered_map<std::string, std::shared_ptr<Species>> all_species;
+    };
 
 }
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include "TGM.h"
 
 int main() {
+
+	tgm::Ecosystem ecosystem;
+	auto bunny = ecosystem.AddSpecies("bunny", 100, tgm::Genes{ 0.2f,0.3f,0.5f });
+	auto wolf = ecosystem.AddSpecies("wolf", 100, tgm::Genes{ 0.2f,0.3f,0.5f });
+
+	ecosystem.PrintSpeciesPopulations();
+
 	while (true) {
 		std::cin.get();
+		ecosystem.Update();
+		ecosystem.PrintSpeciesPopulations();
 	}
 }

cea4d259be88a7adb32e9fac57b80e4467047d63
Martin Starkov <emailregisterlogin@gmail.com>
2020-08-31 04:09:15 +0300

    change trial counts

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
 	auto bunny = ecosystem.AddSpecies("bunny", 1000, tgm::Genes{ 0.12f, 0.12f, 0.01f, 0.0001f }, tgm::Resources{});
 	//auto wolf = ecosystem.AddSpecies("wolf", 100, tgm::Genes{ 0.2f,0.3f,0.5f });
 
-	tgm::internal::MonteCarlo(ecosystem, 1000, 100);
+	tgm::internal::MonteCarlo(ecosystem, 100, 700);
 
 	/*
 	while (true) {

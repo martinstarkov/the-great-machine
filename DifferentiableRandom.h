e8544b1d2fcb61a40cc8adc1256660e2e8907f24
Martin Starkov <emailregisterlogin@gmail.com>
2020-09-03 00:49:31 +0300

    Move some internals to utility header

--- DifferentiableRandom.h
+++ DifferentiableRandom.h
@@ -1,28 +1,6 @@
 #pragma once
 
-#include <random>
-#include <cassert>
-
-namespace internal {
-    template <typename Floating,
-        std::enable_if_t<std::is_floating_point<Floating>::value, int> = 0
-    >
-        Floating GetRandomValue(Floating min_range, Floating max_range) {
-        std::random_device rd;
-        std::mt19937 rng(rd());
-        std::uniform_real_distribution<Floating> distribution(min_range, max_range);
-        return distribution(rng);
-    }
-    template <typename Integer,
-        std::enable_if_t<std::is_integral<Integer>::value, int> = 0
-    >
-        Integer GetRandomValue(Integer min_range, Integer max_range) {
-        std::random_device rd;
-        std::mt19937 rng(rd());
-        std::uniform_int_distribution<Integer> distribution(min_range, max_range);
-        return distribution(rng);
-    }
-}
+#include <Engine/Utility.h>
 
 template <typename T, typename D = double, std::enable_if_t<std::is_floating_point<T>::value || std::is_integral<T>::value, int> = 0>
 class DifferentiableRandom {

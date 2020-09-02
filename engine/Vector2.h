7a593a0f23112c8a8589b8f3e0cf5bf282f8cb38
Martin Starkov <emailregisterlogin@gmail.com>
2020-09-03 00:49:01 +0300

    Create Vector2.h

---
+++ engine/Vector2.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <type_traits>
+
+template <typename T, std::enable_if_t<std::is_floating_point<T>::value || std::is_integral<T>::value, int> = 0>
+struct Vector2 {
+	T x = 0, y = 0;
+	Vector2() = default;
+	Vector2(T x, T y) : x(x), y(y) {}
+	Vector2(const Vector2&) = default;
+	Vector2& operator=(const Vector2&) = default;
+	Vector2(Vector2&&) = default;
+	Vector2& operator=(Vector2&&) = default;
+	friend std::ostream& operator<<(std::ostream& os, const Vector2& obj) {
+		os << "(" << obj.x << "," << obj.y << ")";
+		return os;
+	}
+	static Vector2 Random(T min_x, T max_x, T min_y, T max_y) {
+		return { internal::GetRandomValue<T>(min_x, max_x), internal::GetRandomValue<T>(min_y, max_y) };
+	}
+};
+
+template <typename T>
+inline Vector2<T> operator*(const Vector2<T>& lhs, const Vector2<T>& rhs) {
+	return Vector2<T>{ lhs.x * rhs.x, lhs.y * rhs.y };
+}
+
+using V2_int = Vector2<int>;
+using V2_float = Vector2<float>;
+using V2_double = Vector2<double>;

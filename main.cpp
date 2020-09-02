f9061a37993ba065d3d2998242b95ae13e499442
Martin Starkov <emailregisterlogin@gmail.com>
2020-09-02 08:53:36 +0300

    Test different configurations

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
 
 	tgm::Ecosystem ecosystem;
 	auto testJson = json::JsonObject{ "test.json" };
+	LOG(testJson);
 	//auto bunny = ecosystem.AddSpecies("bunny", 1000, tgm::Genes{ 0.12f, 0.12f, 0.01f, 0.0001f }, tgm::Resources{});
 	//auto wolf = ecosystem.AddSpecies("wolf", 100, tgm::Genes{ 0.2f,0.3f,0.5f });
 

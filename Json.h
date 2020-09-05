9e2b801ce89a37f306493af606fc1b7ee336dfeb
Nicole Burg <32913338+nicolerburg@users.noreply.github.com>
2020-09-05 14:02:45 -0400

    NOT WORKING: New parsing method

--- Json.h
+++ Json.h
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <sstream>
 #include <variant>
+#include <array>
 #include "TGM.h"
 
 
@@ -52,6 +53,20 @@ namespace internal {
 		}
 	}
 
+	std::size_t FindNthOccur(const std::string& data, const std::size_t start, const std::size_t end, char character, std::size_t N) {
+		
+		std::size_t occurance = 0;
+
+		for (auto i = start; i < end; i++) {
+			if (data[i] == character) {
+				occurance += 1;
+			}
+			if (occurance == N)
+				return i;
+		}
+		return -1;
+	}
+
 	// Thanks to bolov for Variant Cast https://stackoverflow.com/a/47204507
 
 	template <class... Args>
@@ -85,11 +100,11 @@ namespace json {
 
 	using JsonType = std::variant<JsonObject, JsonArray, JsonNull, std::string, int, double, bool>;
 
-	enum class ParseFlag {
-		COMMA = 0,
-		CURLY,
-		BRACKET,
-		END
+	enum class ParseFlag { // numbers are important for determine flag
+		COMMA = 0, //',',
+		CURLY, //= '{',
+		BRACKET, //= '[',
+		END,// = ' '
 	};
 
 	// Following two functions test if the passed string holds needed characters for object/array
@@ -113,9 +128,73 @@ namespace json {
 		auto end = data.find_last_of(end_char);
 		assert(start != std::string::npos);
 		assert(end != std::string::npos);
-		auto object = data.substr(start + 1, end - start - 1);
+		auto object = data.substr(start + 1, end - start);
 		object.erase(std::remove(object.begin(), object.end(), '\n'), object.end());
 		return object;
+	}
+
+	void NestedSearch(const std::string& data, const std::size_t start, std::size_t& end, const ParseFlag flag) {
+		// try to avoid old determine flag function
+		char character_back;
+		char character;
+		if (flag == ParseFlag::CURLY) {
+			character_back = '}';
+			character = '{';
+		} else {
+			character_back = ']';
+			character = '[';
+		}
+
+		auto char_num = std::count(data.begin() + start, data.begin() + end, character);
+		assert(char_num != 0);
+
+		if (char_num == 1) {
+			// done
+		} else if (char_num > 1) {
+			auto new_end = internal::FindNthOccur(data, end, data.length() - 1, character_back, char_num);
+			assert(new_end != -1);
+			end = new_end;
+		}
+
+	}
+
+	std::size_t DetermineFlag(const std::string& data, const std::size_t start, const std::size_t end, ParseFlag& flag) {
+		auto first_char = end;
+		for (auto i = 0; i < 3; ++i) {
+			auto potential_flag = static_cast<ParseFlag>(i);
+
+			std::size_t location;
+			if (potential_flag == ParseFlag::COMMA) location = data.find(',', start);
+			if (potential_flag == ParseFlag::CURLY) location = data.find('{', start);
+			if (potential_flag == ParseFlag::BRACKET) location = data.find('[', start);
+			//auto location = data.find(static_cast<char>(potential_flag), start);
+			if (location == std::string::npos) continue;
+			if (location < first_char) {
+				first_char = location;
+				flag = static_cast<ParseFlag>(i);
+			}
+		}
+		return first_char;
+	}
+
+	void DetermineEnd(const std::string& data, const std::size_t start, std::size_t& end) {
+		ParseFlag flag{ ParseFlag::END };
+		auto new_start = DetermineFlag(data, start, end, flag);
+		switch (flag) {
+			case ParseFlag::COMMA:
+				end = new_start - 1;
+				break;
+			case ParseFlag::CURLY:
+			case ParseFlag::BRACKET:
+				NestedSearch(data, new_start, end, flag);
+				break;
+			case ParseFlag::END:
+				end = new_start - 1; //figure out how this will work
+				break;
+			default:
+				assert(false);
+				break;
+		}
 	}
 
 	class JsonObject {
@@ -142,10 +221,12 @@ namespace json {
 	private:
 		void AddPair(const std::string& key, JsonType value);
 
+		void ParsePairThree(const std::string& data, std::size_t start = 0);
+
 		void ParsePair(const std::string& data, std::size_t start = 0);
 
 		void ParseObject(const std::string& data) {
-			ParsePair(data);
+			ParsePairThree(data);
 		}
 
 		std::string key = "json";
@@ -200,8 +281,27 @@ namespace json {
 
 	}
 
+	void JsonObject::ParsePairThree(const std::string& data, const std::size_t start) {
+		auto end = data.find('}', start);
+		assert(end != std::string::npos);
+		auto colon = data.find(':', start);
+		assert(colon != std::string::npos);
+		DetermineEnd(data, start, end);
+		auto key = data.substr(start, colon - 1);
+		key = internal::Trim(key, ' ');
+		auto value = data.substr(colon + 1, end - (colon + 1));
+		AddPair(key, ParseType(key, value));
+		ParsePairThree(data, end + 1);
+		// if (std::find(std::begin(data) + start, std::begin(data) + end, ',') != std::begin(data) + end) {
+		//	ParsePairThree(data, end + 1);
+		//}
+		// revisit end + 1
+	}
+
+/*
 	// recursive function that checks for nested objects or arrays and returns the location of the outermost end 
-	std::size_t JsonObject::NestedSearch(const std::string& data, ParseFlag& flag) {
+	std::size_t JsonObject::NestedSearch(const std::string& data, ParseFlag& flag, size_t start) {
+		// determines which character to look for
 		char character;
 		char character_back;
 		if (flag == ParseFlag::CURLY) {
@@ -211,14 +311,26 @@ namespace json {
 			character = '[';
 			character_back = ']';
 		}
-		auto open_symbol = data.find(character);
+
+		// finds the location of the first open and closed symbols
+		auto open_symbol = data.find(character, start);
 		if (open_symbol == std::string::npos) open_symbol = data.length() - 1;
-		auto close_symbol = data.find(character_back);
-		if (close_symbol < open_symbol) { // there is no object inside this one
-			return close_symbol;
-		} else { // there is another object
+		auto close_symbol = data.find(character_back, start);
+
+		if (close_symbol <= open_symbol) { // there is a closed before an open
+			// now we count how many open vs close are between start of data and close_symbol
+			auto num_of_open = std::count(data.begin(), data.begin() + close_symbol, character);
+			auto num_of_closed = std::count(data.begin(), data.begin() + close_symbol, character_back);
+			if (num_of_open == num_of_closed) { // we have the whole outer object
+				return close_symbol;
+			} else { //we have a deeper object
+
+			}
+
+		} else { // there is an open before a closed
 			// we need to recurse without losing the location of the final }/] in relation to original data
-			//NestedSearch(data, flag);
+			// search for the next open and close, is the close first?
+			NestedSearch(data, flag, open_symbol);
 		}
 	}
 
@@ -238,39 +350,37 @@ namespace json {
 			if (first_curly < result) {
 				flag = ParseFlag::CURLY;
 				result = first_curly;
+				NestedSearch(data, flag, 0);
 			}
 		}
 		if (first_bracket != std::string::npos) {
 			if (first_bracket < result) {
 				flag = ParseFlag::BRACKET;
 				result = first_bracket;
+				NestedSearch(data, flag, 0);
 			}
 		}
 	}
 
-	void JsonObject::ParsePairTwo(const std::string& data) {
-		auto result = data.length() - 1;
+	void JsonObject::ParsePairTwo(const std::string& data, std::size_t start) {
+		auto end = data.find('}', start); // end is location of found comma, {, or [ 
 		ParseFlag flag{ ParseFlag::END };
 		// first we find the key and save it
 		auto colon = data.find(':');
 		assert(colon != std::string::npos);
-		auto key = data.substr(0, colon);
+		auto key = data.substr(start, colon - start);
 		key = internal::Trim(key, ' ');
 		// now we remove it and focus on the contents
-		data.substr(colon + 1);
-		NextImportant(data, result, flag);
-		if (flag == ParseFlag::COMMA) {
-			auto value = internal::Trim(data.substr(0, result - 1), ' ');
-			ParsePairTwo(internal::Trim(data.substr(result + 1), ' '));
-		}
-		if (flag == ParseFlag::CURLY) {
-			NestedSearch(data, flag);
-		}
-		if (flag == ParseFlag::BRACKET) {
-			NestedSearch(data, flag);
-		}
-	}
+		std::string value;
+		NextImportant(data, end, flag); // sets end to the end of the object
+		value = data.substr(colon + 1, end);
+		value = internal::Trim(value, ' ');
+		ParsePairTwo(data, end + 1);
 
+
+		AddPair(key, ParseType(key, value));
+	}
+	*/
 
 	void JsonObject::ParsePair(const std::string& data, std::size_t start) {
 		auto colon = data.find(':', start);
--- main.cpp
+++ main.cpp
@@ -4,8 +4,13 @@
 int main() {
 
 	tgm::Ecosystem ecosystem;
-	auto testJson = json::JsonObject{ "test.json" };
+
+	auto testJson = json::JsonObject{ "simple.json" };
 	LOG(testJson);
+
+	//auto testJson = json::JsonObject{ "test.json" };
+	//LOG(testJson);
+
 	//auto bunny = ecosystem.AddSpecies("bunny", 1000, tgm::Genes{ 0.12f, 0.12f, 0.01f, 0.0001f }, tgm::Resources{});
 	//auto wolf = ecosystem.AddSpecies("wolf", 100, tgm::Genes{ 0.2f,0.3f,0.5f });
 
